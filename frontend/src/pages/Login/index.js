db1dbabec41ef00c477c618f07485120b40a8c07
Helvecio Neto <helvecioneto77@gmail.com>
2020-03-27 18:34:55 -0300

    Logando na aplicacao

--- frontend/src/pages/Login/index.js
+++ frontend/src/pages/Login/index.js
@@ -1,9 +1,14 @@
-import React, { useRef } from 'react';
-import { Link } from 'react-router-dom';
+import React, { useRef, useState } from 'react';
+import { Link, useHistory } from 'react-router-dom';
 import { Form } from '@unform/web';
+import * as Yup from 'yup';
+import { toast } from 'react-toastify';
 import { FiLogIn } from 'react-icons/fi';
 
-import Input from '../../components/Input';
+import api from '../../services/api';
+
+import Input from '../../components/InputNoMask';
+import Loading from '../../components/Loading';
 
 import { Container } from './styles';
 
@@ -12,13 +17,59 @@ import logo from '../../assets/logo.svg';
 
 export default function Login() {
   const formRef = useRef(null);
+  const history = useHistory();
+
+  const [loading, setLoading] = useState(false);
+
+  async function handleLogin(data) {
+    try {
+      formRef.current.setErrors({});
+
+      const schema = Yup.object().shape({
+        id: Yup.string().required('ID é obrigatório'),
+      });
+
+      await schema.validate(data, {
+        abortEarly: false,
+      });
+
+      setLoading(true);
+
+      api
+        .post('sessions', data)
+        .then((response) => {
+          setLoading(false);
+          localStorage.setItem('ongId', data.id);
+          localStorage.setItem('ongName', response.data.name);
+
+          history.push('/profile');
+        })
+        .catch((err) => {
+          setLoading(false);
+          toast.error(
+            err.response.data.error ||
+              'Erro ao fazer login, verifique seus dados!'
+          );
+        });
+    } catch (err) {
+      const validationErrors = {};
+
+      if (err instanceof Yup.ValidationError) {
+        err.inner.forEach((error) => {
+          validationErrors[error.path] = error.message;
+        });
+
+        formRef.current.setErrors(validationErrors);
+      }
+    }
+  }
 
   return (
     <Container>
       <section>
         <img src={logo} alt="Be The Hero" />
 
-        <Form ref={formRef}>
+        <Form onSubmit={handleLogin} ref={formRef}>
           <h1>Faça seu logon</h1>
 
           <Input
@@ -28,7 +79,9 @@ export default function Login() {
             style={{ height: 60 }}
           />
 
-          <button type="submit">Entrar</button>
+          <button type="submit">
+            {loading ? <Loading size={25} color="#fff" /> : 'ENTRAR'}
+          </button>
 
           <Link to="/register">
             <FiLogIn size={16} color="#E02041" />
