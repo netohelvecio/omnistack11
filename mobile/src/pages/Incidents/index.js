0bba7c8dee68b67a42b8429c71dcf8749c92fb36
Helvecio Neto <helvecioneto77@gmail.com>
2020-03-28 17:05:41 -0300

    Estilizando pagina de casos

--- mobile/src/pages/Incidents/index.js
+++ mobile/src/pages/Incidents/index.js
@@ -1,12 +1,65 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { Image } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import { Feather } from '@expo/vector-icons';
 
-// import { Container } from './styles';
+import {
+  Container,
+  Header,
+  HeaderText,
+  HeaderTextBold,
+  Title,
+  Description,
+  IncicedentsList,
+  Incident,
+  Property,
+  Value,
+  DetailsButton,
+  DetailsButtonText,
+} from './styles';
+
+import Logo from '../../assets/logo.png';
 
 export default function Incidents() {
+  const navigation = useNavigation();
+
+  function navigateToDetail() {
+    navigation.navigate('Detail');
+  }
+
   return (
-    <View>
-      <Text>Incidents</Text>
-    </View>
+    <Container>
+      <Header>
+        <Image source={Logo} />
+        <HeaderText>
+          Total de <HeaderTextBold>0 casos</HeaderTextBold>
+        </HeaderText>
+      </Header>
+
+      <Title>Bem-Vindo(a)</Title>
+      <Description>Escolha um dos casos abaixo e salve o dia</Description>
+
+      <IncicedentsList
+        data={[1, 2, 3]}
+        keyExtractor={(incident) => incident.toString()}
+        renderItem={() => (
+          <Incident>
+            <Property>ONG:</Property>
+            <Value>APAD</Value>
+
+            <Property>Caso:</Property>
+            <Value>Cachorro perdido</Value>
+
+            <Property>Valor:</Property>
+            <Value>R$ 120,00</Value>
+
+            <DetailsButton onPress={navigateToDetail}>
+              <DetailsButtonText>Ver mais detalhes</DetailsButtonText>
+              <Feather name="arrow-right" size={20} color="#e02041" />
+            </DetailsButton>
+          </Incident>
+        )}
+      />
+    </Container>
   );
 }
--- mobile/src/pages/Incidents/styles.js
+++ mobile/src/pages/Incidents/styles.js
@@ -1,3 +1,75 @@
 import styled from 'styled-components/native';
+import Constants from 'expo-constants';
 
-export const Container = styled.View``;
+export const Container = styled.View`
+  flex: 1;
+  padding: 0 24px;
+  padding-top: ${Constants.statusBarHeight + 20};
+`;
+
+export const Header = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export const HeaderText = styled.Text`
+  font-size: 15px;
+  color: #737380;
+`;
+
+export const HeaderTextBold = styled.Text`
+  font-weight: bold;
+`;
+
+export const Title = styled.Text`
+  font-size: 30px;
+  margin-bottom: 16px;
+  margin-top: 28px;
+  color: #13131a;
+  font-weight: bold;
+`;
+
+export const Description = styled.Text`
+  font-size: 16px;
+  line-height: 24px;
+  color: #737380;
+`;
+
+export const IncicedentsList = styled.FlatList.attrs({
+  showsVerticalScrollIndicator: false,
+})`
+  margin-top: 32px;
+`;
+
+export const Incident = styled.View`
+  padding: 24px;
+  border-radius: 8px;
+  background: #fff;
+  margin-bottom: 16px;
+`;
+
+export const Property = styled.Text`
+  font-size: 14px;
+  color: #41414d;
+  font-weight: bold;
+`;
+
+export const Value = styled.Text`
+  margin-top: 8px;
+  margin-bottom: 24px;
+  font-size: 15px;
+  color: #737380;
+`;
+
+export const DetailsButton = styled.TouchableOpacity`
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export const DetailsButtonText = styled.Text`
+  color: #e02041;
+  font-weight: bold;
+  font-size: 15px;
+`;
